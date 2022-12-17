d844c11a97717bde9a6ebf6586450cbd70900c9b
tomonariasai <t.asai@4nonome.com>
2022-12-17 15:46:07 +0900

    Atoms/Alert

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -2,17 +2,23 @@ import styled from "styled-components";
 import React from "react";
 import COLOR from "../../../variables/color";
 import TEXT from "../../../variables/texts";
-import FONTFAMILY from "../../../variables/font_family";
 import BREAKPOINT from "../../../variables/breakpoint";
 
 const Alert = ({ alertText, visible }) => {
   return (
-    <StyledAlert isActive={visible}>
-      <StyledalertText>{alertText}</StyledalertText>
-    </StyledAlert>
+    <ALertContainer>
+      <StyledAlert isActive={visible}>
+        <StyledalertText>{alertText}</StyledalertText>
+      </StyledAlert>
+    </ALertContainer>
   );
 };
 export default Alert;
+
+const ALertContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const StyledAlert = styled.div`
   width: 400px;
@@ -21,13 +27,16 @@ const StyledAlert = styled.div`
   padding: 10px 20px;
   border-radius: 4px;
   transition: 0.5s all ease;
-  transform: translateY(${(props) => (props.isActive ? 20 : 5)}px);
+  transform: translateY(${(props) => (props.isActive ? 80 : 60)}px);
+  opacity: ${(props) => (props.isActive ? 1 : 0)};
+  @media screen and (max-width: ${BREAKPOINT.MEDIUM}) {
+    transform: translateY(${(props) => (props.isActive ? 40 : 20)}px);
+    pading: 0px 20px;
+    width: 100%;
+  }
 `;
-// opacity: ${(props) => (props.isActive ? 1 : 0)};
 
 const StyledalertText = styled.div`
   color: ${COLOR.WHITE};
   ${TEXT.S}
-  @media screen and (max-width: ${BREAKPOINT.MEDIUM}) {
-  }
 `;
